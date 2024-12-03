a11bae31264c44ad216702c36ce7e24904feb08c
Steven-Roduta <stevenroduta00@gmail.com>
2024-12-03 11:34:06 +0100

    add two prompt

---
+++ jsnack1/script.js
@@ -0,0 +1,5 @@
+// chiedo l' utente di inserire due numeri in successione
+const utente = prompt ('Inserisci due numeri in successione')
+// il software deve stampare il numero maggiore
+
+const utenteSoft = prompt ('Stampa il numero maggiore')
