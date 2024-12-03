69bd0ecfe6fe90e03633bc6240969d5063b3d95f
Steven-Roduta <stevenroduta00@gmail.com>
2024-12-03 11:46:53 +0100

    add if and else

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -5,3 +5,11 @@ const utente = prompt ('Inserisci due numeri in successione');
 
 const utenteSoft = prompt ('Stampa il numero maggiore');
 console.log (utente , utenteSoft);
+
+if (utente >= utenteSoft ) {
+   
+    result = 'Stampa'
+} else {
+
+    result = 'non Stampare'
+}
