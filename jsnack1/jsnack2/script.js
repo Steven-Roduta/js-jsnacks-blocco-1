38b07ad54110f553d8f62bf0d15a09258652e0d7
Steven-Roduta <stevenroduta00@gmail.com>
2024-12-03 12:30:29 +0100

    feat add js

---
+++ jsnack1/jsnack2/script.js
@@ -0,0 +1,26 @@
+// Nomi Invitati di Gatsby
+const names = [
+    'Goku',
+    'Vegeta',
+    'Chichi',
+    'Gohan',
+    'Gotens',
+    'Trunks',
+    'Piccolo',
+]
+// chiedi all' utente il suo nome
+
+const utenteName = prompt ('Put your Name');
+
+// se l' utente puo' partecipare oppure
+
+for ( let i = 0; i = utenteName.length ; i++){
+
+    const nameInvited = names [i];
+
+    if (nameInvited.length = names){
+        utenteName.push (names)
+        console.log(utenteName,names);
+    }
+
+}
--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,9 +1,9 @@
 // chiedo l' utente di inserire due numeri in successione
-const utente = prompt ('Inserisci due numeri in successione');
+const utente = Number (prompt ('Inserisci due numeri in successione'));
 
 // il software deve stampare il numero maggiore
 
-const utenteSoft = prompt ('Stampa il numero maggiore');
+const utenteSoft = Number ( prompt ('Stampa il numero maggiore'));
 console.log (utente , utenteSoft);
 
 if (utente >= utenteSoft ) {
