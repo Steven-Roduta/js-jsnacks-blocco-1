95bdf8b0a68fa8537a769b119bf21384f9010a17
Steven-Roduta <stevenroduta00@gmail.com>
2024-12-03 11:36:48 +0100

    add console log

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,5 +1,7 @@
 // chiedo l' utente di inserire due numeri in successione
-const utente = prompt ('Inserisci due numeri in successione')
+const utente = prompt ('Inserisci due numeri in successione');
+
 // il software deve stampare il numero maggiore
 
-const utenteSoft = prompt ('Stampa il numero maggiore')
+const utenteSoft = prompt ('Stampa il numero maggiore');
+console.log (utente , utenteSoft);
